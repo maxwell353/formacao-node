9ecc7e5bac875f5ffb2fcdbf6fdfa2c20e078761
Maxwell Alves <maxwell.alves.ced@prof.ced.ce.gov.br>
2022-03-10 15:16:56 -0300

    Aula 5 finalizada

--- lib-markdown/cli.js
+++ lib-markdown/cli.js
@@ -1,11 +1,16 @@
 import yellow from 'chalk';
 import pegaArquivo from './index.js';
+import validaURLs from './http-validacao.js';
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDeArquivo) {
   const resultado = await pegaArquivo(caminhoDeArquivo[2]);
-  console.log(yellow('lista de links'), resultado);
+  if (caminhoDeArquivo[3] === 'validar') {
+    console.log(yellow('links validados'), await validaURLs(resultado));
+  } else {
+    console.log(yellow('lista de links'), resultado);
+  }
 }
 
 processaTexto(caminho);
